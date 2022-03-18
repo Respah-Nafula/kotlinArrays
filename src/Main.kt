62439161c56fa75ccb5b8df28dc22ee966885abf
Respah-Nafula <nafularespah@gmail.com>
2022-03-18 23:05:02 +0300

    first commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,29 +1,37 @@
 fun main(){
-siblings(arrayOf("Leah","Rebeccah","Respah","Eunice"))
-city(arrayOf())
-    numbers(arrayOf())
-   var all =names("Naomi","Sharon","Pauline")
-    println(all)
+    var x=arrays(arrayOf(50,15,30,25))
+    println(x)
+var w=mixedtypes(arrayOf("Naomi",456,98.4,67.8,true))
+    println(w)
+    var n=characters(arrayOf('e','s','r','u','a','w','i'))
+    println(n)
 
-}
-fun siblings(sib:Array<String>){
-println(sib.contentToString())
-}
-fun city(places:Array<String>){
-    var cities=arrayOf("harare","mumbai","dodoma","jakarta")
-    for (d in cities)
-        println(d.capitalize())
-}
-fun numbers(nums:Array<String>){
-    var digits= arrayOf(32,17,4,213,78,43,90,31,373,11,158,62)
-    println(digits[1]+digits[4])
-    println(digits.indexOf(158))
-    println(digits.sorted())
-}
+    }
 
-fun names(c: String,d:String,e:String):String{
-    var texts= arrayOf(c,d,e)
-    var friends=texts.contentToString()
-    return friends
+fun arrays(numbers:Array<Int>): Int{
+    var product=1
+    numbers.forEach { number->
+        product*=number
+    }
+    return product
+}
+fun mixedtypes(digits:Array<Any>) :Double{
+    var sum=0.0
+    digits.forEach { number->
+        if (number is Int|| number is Double)
+    sum+=number.toString().toDouble()
+
+        }
+     return sum
+    }
+fun characters(chars:Array<Char>): Int {
+    var vowels=0
+    chars.forEach { r->
+        if (r=='a'||r=='e'||r=='i' ||r=='o'||r=='u'){
+          vowels++
+             }
+         }
+        return vowels
+
      }
 
